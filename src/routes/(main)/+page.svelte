f91558dfaa4366edeaf7703675756918e6c0c479
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2025-08-11 18:38:22 +0100

    feat(home): add scroll bottom arrow

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -51,3 +51,26 @@
     </div>
   </div>
 </div>
+
+<div class="bg-dark pt-6">
+  <div class="container flex justify-center">
+    <div class="animate-bounce">
+      <span class="text-primary text-2xl cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m19.5 8.25-7.5 7.5-7.5-7.5"
+          />
+        </svg>
+      </span>
+    </div>
+  </div>
+</div>

bfa0e556ea71092598b5d48912c7c7f4925e13ba
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2025-09-28 13:23:11 +0100

    fix: rename og image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,6 @@
     content="I am a passionate Software Engineer with experience in designing, developing, and managing web projects. Proficient in JavaScript, TypeScript (Node.js, Express, SvelteKit, React, Next.js), PHP, and HTML, CSS (Tailwind). Strong expertise in Object-Oriented Programming (OOP) and skilled in leveraging various tools and databases to build robust, scalable, and efficient solutions."
   />
   <meta name="og:image" content="https://henen-dev.vercel.app/og-image.png" />
-  <meta name="twitter:card" content="summary_large_image" />
 
   <link rel="icon" href={favicon} />
 </svelte:head>

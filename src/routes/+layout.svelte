34dc86e4e81df5d2b2074aa57e727579a2091d7a
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2025-09-28 13:21:36 +0100

    feat: update description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,12 +9,12 @@
   <title>Moses Henen Developer — Portfolio</title>
   <meta
     name="description"
-    content="I am a passionate Software Engineer with 3 years of experience in designing, developing, and managing web projects. Proficient in JavaScript, TypeScript (Node.js, Express, SvelteKit), PHP (Laravel), and HTML, CSS (Tailwind). Strong expertise in Object-Oriented Programming (OOP) and skilled in leveraging various tools and databases to build robust, scalable, and efficient solutions."
+    content="I am a passionate Software Engineer with experience in designing, developing, and managing web projects. Proficient in JavaScript, TypeScript (Node.js, Express, SvelteKit, React, Next.js), PHP, and HTML, CSS (Tailwind). Strong expertise in Object-Oriented Programming (OOP) and skilled in leveraging various tools and databases to build robust, scalable, and efficient solutions."
   />
   <meta name="og:title" content="Moses Henen Developer — Portfolio" />
   <meta
     name="og:description"
-    content="I am a passionate Software Engineer with 3 years of experience in designing, developing, and managing web projects. Proficient in JavaScript, TypeScript (Node.js, Express, SvelteKit), PHP (Laravel), and HTML, CSS (Tailwind). Strong expertise in Object-Oriented Programming (OOP) and skilled in leveraging various tools and databases to build robust, scalable, and efficient solutions."
+    content="I am a passionate Software Engineer with experience in designing, developing, and managing web projects. Proficient in JavaScript, TypeScript (Node.js, Express, SvelteKit, React, Next.js), PHP, and HTML, CSS (Tailwind). Strong expertise in Object-Oriented Programming (OOP) and skilled in leveraging various tools and databases to build robust, scalable, and efficient solutions."
   />
   <meta name="og:image" content="https://henen-dev.vercel.app/og-image.png" />
   <meta name="twitter:card" content="summary_large_image" />
